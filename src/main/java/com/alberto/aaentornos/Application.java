f616f2a12b52cf9a7f7ff8ffeb3f2c759bd2fb17
albertrodtab <albertrodtab@gmail.com>
2021-04-02 17:19:17 +0200

    Diseño el menú para poder visualizar objetos de las distintas clases

--- src/main/java/com/alberto/aaentornos/Application.java
+++ src/main/java/com/alberto/aaentornos/Application.java
@@ -2,7 +2,8 @@ package com.alberto.aaentornos;
 
 public class Application {
     public static void main (String[] args) {
-
+        Gestion gestion = new Gestion();
+        gestion.ejecutar();
     }
 
 }
